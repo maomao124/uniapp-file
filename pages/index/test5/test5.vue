0ad608738bf4bc486f6d25ae9617733268edc67d
mao <1296193245@qq.com>
2023-12-31 21:00:01 +0800

    uniapp-file

---
+++ pages/index/test5/test5.vue
@@ -0,0 +1,118 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">选择文件保存</button>
+		<button type="primary" @click="button2">查看已保存的文件</button>
+		<text>文件列表：</text>
+		<view v-for="file in fileList" :key="file.filePath">
+			<button type="warn" @click="button3(file.filePath)">删除：{{file.filePath}}</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				fileList:[],
+			}
+		},
+		methods: {
+			button1() {
+				uni.chooseImage({
+					success: function(res) {
+						var tempFilePaths = res.tempFilePaths;
+						uni.saveFile({
+							tempFilePath: tempFilePaths[0],
+							success: function(res) {
+								var savedFilePath = res.savedFilePath;
+								console.log("保存文件路径：",savedFilePath);
+							},
+							fail: (err) => {
+								console.log("文件保存失败：",err);
+							}
+						});
+					}
+				});
+			},
+			button2() {
+				uni.getSavedFileList({
+					success: (data) => {
+						console.log(data.fileList);
+						this.fileList=data.fileList;
+					},
+					fail: (err) => {
+						console.log("获取文件列表失败：",err);
+					}
+				})
+			},
+			button3(path)
+			{
+				console.log(path);
+				uni.getSavedFileInfo({
+					filePath:path,
+					success: (data) => {
+						console.log(data);
+						uni.showModal({
+							title:'删除提示',
+							content:'是否删除以下文件：'+JSON.stringify(data),
+							success: (res) => {
+								if(res.confirm)
+								{
+									uni.removeSavedFile({
+										filePath:path,
+										success: (res) => {
+											uni.showModal({
+												showCancel:false,
+												title:'提示',
+												content:'删除成功!'
+											})
+											this.button2();
+										},
+										fail: (err) => {
+											uni.showModal({
+												showCancel:false,
+												title:'错误',
+												content:'文件删除失败!'
+											})
+										}
+									})
+								}
+							}
+						})
+					},
+					fail(err) {
+						console.log(err);
+						uni.showModal({
+							title:'错误',
+							content:'查看文件信息失败!'
+						})
+					}
+				})
+			}
+		},
+		onShow() {
+			uni.getSavedFileList({
+				success: (data) => {
+					console.log(data.fileList);
+					this.fileList=data.fileList;
+				},
+				fail: (err) => {
+					uni.showModal({
+						title:'错误',
+						content:'读取文件列表失败!'
+					})
+				}
+			})
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 5px;
+}
+text{
+	color: coral;
+	font-size: 1.5em;
+}
+</style>
